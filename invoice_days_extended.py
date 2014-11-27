5996eb56b38da1e55e865aa74ca971e9c72eb6ea
Business Tec Systems <BusinessTec@users.noreply.github.com>
2014-11-27 17:45:45 -0600

    Update invoice_days_extended.py

--- invoice_days_extended.py
+++ invoice_days_extended.py
@@ -6,11 +6,11 @@ from datetime import datetime
 class invoice_days(models.Model):
 	
 	_inherit = "account.invoice"
-	over_days = fields.Integer(string='Atrasado',compute="days_over",readonly=True)
+	over_days = fields.Integer(string='Atrasado',compute="_days_over",readonly=True)
 
 	@api.one
 	@api.depends('state','date_due','date_invoice')
-	def days_over(self):
+	def _days_over(self):
 		if self.state == 'open'
 			if payment_term:
 				self.over_days=(today-self.date_due).days
